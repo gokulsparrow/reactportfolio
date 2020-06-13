6d282dc84daf270d8e4cdb952dd4ca5eb6a7a65e
gokulsparrow <59665728+gokulsparrow@users.noreply.github.com>
2020-06-13 19:27:48 +0530

    Updates

---
+++ precache-manifest.553803e9598a1e39c4e0fadf247ac0b2.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b678d2ee6aa1a194ab9f1be3ad5870f4",
+    "url": "/reactportfolio/index.html"
+  },
+  {
+    "revision": "6d7291c4120dfba97e75",
+    "url": "/reactportfolio/static/css/2.74189dbb.chunk.css"
+  },
+  {
+    "revision": "fd59913e9a8ca6a80a3e",
+    "url": "/reactportfolio/static/css/main.08779183.chunk.css"
+  },
+  {
+    "revision": "6d7291c4120dfba97e75",
+    "url": "/reactportfolio/static/js/2.16114e3d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/reactportfolio/static/js/2.16114e3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd59913e9a8ca6a80a3e",
+    "url": "/reactportfolio/static/js/main.58f4565a.chunk.js"
+  },
+  {
+    "revision": "5fc6824b9569be253d45",
+    "url": "/reactportfolio/static/js/runtime-main.3df5a9e3.js"
+  },
+  {
+    "revision": "6bea1566a9569b065a67a30e894e0f17",
+    "url": "/reactportfolio/static/media/fire.6bea1566.jpg"
+  },
+  {
+    "revision": "68555816f89ef19853929b11787c3c20",
+    "url": "/reactportfolio/static/media/person.68555816.jpg"
+  },
+  {
+    "revision": "e86276ac18cac4ce2cd296a27003a633",
+    "url": "/reactportfolio/static/media/weber.e86276ac.jpg"
+  }
+]);
